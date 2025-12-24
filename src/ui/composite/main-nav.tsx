abce983c11505e14a099ed4841d69ab5967db091
thatKwon <thatKwon@gmail.com>
2025-12-24 16:15:07 +0900

    commit

--- src/ui/composite/main-nav.tsx
+++ src/ui/composite/main-nav.tsx
@@ -22,11 +22,11 @@ const links = [
 
 const mobileLinks = [
   { href: "/home", title: "홈페이지", type: "logo" },
-  { href: "/team", title: "회사 소개", type: "text" },
-  { href: "/project", title: "시공 후기", type: "text" },
-  { href: "/support", title: "고객 지원", type: "text" },
-  { href: "tel:010-46885-9699", title: "전화 연결", type: "text" },
-  { href: "/contact", title: "견적 문의", type: "button" },
+  { href: "/team", title: "회사소개", type: "text" },
+  { href: "/project", title: "시공후기", type: "text" },
+  { href: "/support", title: "고객지원", type: "text" },
+  { href: "tel:010-46885-9699", title: "전화연결", type: "text" },
+  { href: "/contact", title: "견적문의", type: "button" },
 ];
 
 export const MainNav = () => (
