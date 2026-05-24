69115810f2fa36c0711ed01f295811933578153e
thatKwon <thatKwon@gmail.com>
2026-05-24 20:42:32 +0900

    commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,12 @@
 ﻿import { ContactHeroContainer } from "@/ui/composite/contact-hero-container";
 import { ContactQuestionContainer } from "@/ui/composite/contact-question-container";
-import { ContactEstimateContainer } from "@/ui/composite/contact-estimate-container";
 
 export default function Contact() {
   return (
     <main className="mb-120 flex flex-col items-center justify-start gap-y-[clamp(2rem,4vw,4rem)]">
       <ContactHeroContainer />
       <ContactQuestionContainer />
-      <ContactEstimateContainer />
+      {/*<ContactEstimateContainer />*/}
     </main>
   );
 }
